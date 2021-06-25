6108de40483115ba885bfac2f5984bca5d5aa94c
Syahrial Agni Prasetya <lemniskett@protonmail.com>
2021-06-26 02:12:48 +0700

    Remove build script

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-KERNEL_NAME="Dark-Ages-Tweaks"
-RELEASE="Ultimo"
-DATE="$(date "+%d%m%Y-%I%M")"
-out="${KERNEL_NAME}-${RELEASE}-${DATE}.zip"
-
-zip -r9 "$out" . -x build.sh "*.git*" "${KERNEL_NAME}*"
