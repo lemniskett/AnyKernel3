c95b01bb042b1a451d6258ed9f1592d5a2ea708e
Syahrial Agni Prasetya <lemniskett@protonmail.com>
2021-07-14 23:01:51 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Dark-Ages Kernel By Blacksuan19, Tweaked by Lemniskett
+kernel.string=Redmi 5 Plus kernel
 do.devicecheck=1
 do.modules=0
 do.systemless=0
